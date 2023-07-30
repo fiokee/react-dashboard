e200025282cdde1e341b020558bbc9b249835d49
fiokee <smleffiong@gmail.com>
2023-07-30 23:49:28 +0100

    added icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,9 @@ import {BsPeopleFill} from 'react-icons/bs';
 import {MdOutlineInventory2} from 'react-icons/md';
 import {AiOutlineMessage} from 'react-icons/ai';
 import {FiSettings} from 'react-icons/fi';
+import {SlEarphones} from 'react-icons/sl';
+import {HiOutlineLogout} from 'react-icons/hi';
+import {FiGift} from 'react-icons/fi';
 import Logo from '../assets/logo.png'
 
 import './Header.css'
@@ -23,52 +26,87 @@ const Header = () => {
      <div className="hamburger">
      <i className="fas fa-bars"><FaBars/></i>
      </div>
+     <div>
+      <p>shop</p>
+      <span>Bell</span>
+      <img/>
+     </div>
    </div>
 
-</div>
+  </div>
     <div className="sidebar">
     <div className="sidebar_inner">
   <ul>
       <li>
       <a href="#">
-    <span className="icon"><LuLayoutDashboard/></span>
-    <span className="text">Dashboard</span>
+      <span className="icon"><LuLayoutDashboard/></span>
+      <span className="text">Dashboard</span>
       </a>
       </li>
 
       <li>
       <a href="#">
-    <span className="icon"><i className="fa fa-link"><BiSolidShoppingBag/></i></span>
-    <span className="text">Orders</span>
+      <span className="icon"><i className="fa fa-link"><BiSolidShoppingBag/></i></span>
+      <span className="text">Orders</span>
       </a>
       </li>
+
       <li>
       <a href="#">
-    <span className="icon"><i className="fa fa-eye"><BsPeopleFill/></i></span>
-    <span className="text">Customers</span>
+      <span className="icon"><i className="fa fa-eye"><BsPeopleFill/></i></span>
+      <span className="text">Customers</span>
       </a>
       </li>
+
       <li>
       <a href="#">
-    <span className="icon"><i className="fa fa-book"><MdOutlineInventory2/></i></span>
-    <span className="text">Inventory</span>
+      <span className="icon"><i className="fa fa-book"><MdOutlineInventory2/></i></span>
+      <span className="text">Inventory</span>
       </a>
       </li>
+
       <li>
       <a href="#">
-    <span className="icon"><i className="fa fa-question-circle"><AiOutlineMessage/></i></span>
-    <span className="text">Conversations</span>
+      <span className="icon"><i className="fa fa-question-circle"><AiOutlineMessage/></i></span>
+      <span className="text">Conversations</span>
       </a>
       </li>
+
       <li>
       <a href="#">
-    <span className="icon"><i className="fa fa-pen"><FiSettings/></i></span>
-    <span className="text">Settings</span>
+      <span className="icon"><i className="fa fa-pen"><FiSettings/></i></span>
+      <span className="text">Settings</span>
       </a>
       </li>
   </ul>
 </div>
-..
+
+{/* supports */}
+  <div className='container'>
+    <ul>
+    <li>
+      <a href="#">
+      <span className="icon"><SlEarphones/></span>
+      <span className="text">Contact Support</span>
+      </a>
+      </li>
+
+      <li>
+      <a href="#">
+      <span className="icon"><FiGift/></span>
+      <p className="text">Free Gift Awaits You!</p>
+      <span>Upgrade Your account kk</span>
+      </a>
+      </li>
+
+      <li>
+      <a href="#">
+    <span className="icon"><HiOutlineLogout/></span>
+    <span className="text">Logout ll</span>
+      </a>
+      </li>
+    </ul>
+  </div>
     </div>
     </div>
   )
